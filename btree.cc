cf0ab49c2d114c18171dddecc5b1a1adccb03d2c
jerryli27 <jerrylijiaming@gmail.com>
2015-11-18 16:38:27 -0600

    Shitty first attempt

--- btree.cc
+++ btree.cc
@@ -353,11 +353,191 @@ ERROR_T BTreeIndex::Lookup(const KEY_T &key, VALUE_T &value)
   return LookupOrUpdateInternal(superblock.info.rootnode, BTREE_OP_LOOKUP, key, value);
 }
 
+// The function to split an internal node. The input is the node, the key and the value to be inserted after/during the split
+ERROR_T BTreeIndex::SplitInternal(const SIZE_T &node, const KEY_T &key, const VALUE_T &value){
+  //        Splitting an internal node:
+  //            The difference is xl contains [m-2]-1 smallest keys and xr contains [m/2] largest keys. 
+  //            Note that the [m/2]th key J is not placed in xl or xr, but is used to be a key in parent node
+  //            Make J the parent of xl and xr, and push j together with its child pointers(to xl) into the parent of x. 
+  BTreeNode b;
+  ERROR_T rc;
+  SIZE_T offset;
+  SIZE_T offset_keyvalue;
+  KEY_T testkey;
+  KeyValuePair testkeyvalue;
+  SIZE_T ptr;
+  SIZE_T xl_ptr;
+  SIZE_T xr_ptr;
+  BTreeNode xl_node;
+  BTreeNode xr_node;
+
+  rc= b.Unserialize(buffercache,node);
+
+  if (rc!=ERROR_NOERROR) { 
+    return rc;
+  }
+  //check whether the node is internal node
+  assert(b.info.nodetype==BTREE_ROOT_NODE || b.info.nodetype==BTREE_INTERIOR_NODE);
+  // Find the position to insert key and value.
+  for (offset=0;offset<b.info.numkeys;offset++) { 
+    rc=b.GetKey(offset,testkey);
+    if (rc) {  return rc; }
+    if (key<testkey || key==testkey) {
+      // OK, so we now have the first key that's larger
+      // So we record the offset and quit
+      offset_keyvalue=offset;
+      break;
+    }
+  }
+  if (offset_keyvalue<b.info.numkeys/2-1){
+    // If the key to be inserted is in xl
+    rc=b.GetPtr(offset,ptr);
+    if (rc) { return rc; }
+    // Allocate a new node. (The left node)
+    rc=AllocateNode(&xl_ptr);
+    if (rc) { return rc; }
+    rc=xl_node.Unserialize(buffercache,xl_ptr);
+    if (rc) { return rc; }
+    // Loop through the original node and insert the first [m-2]-1 smallest key/value pairs
+    for (offset=0;offset<offset_keyvalue;offset++) { 
+      rc=b.GetKeyVal(offset,testkeyvalue);
+      if (rc) {  return rc; }
+      xl_node.SetKeyVal(offset,testkeyvalue);
+    }
+    //insert the input key/value pair
+    xl_node.SetKey(offset_keyvalue,key);
+    xl_node.SetVal(offset_keyvalue,value);
+    // Keep doing the loop and insert the first [m-2]-1 smallest key/value pairs
+    for (offset=offset_keyvalue;offset<b.info.numkeys/2-1;offset++) { 
+      rc=b.GetKeyVal(offset,testkeyvalue);
+      if (rc) {  return rc; }
+      xl_node.SetKeyVal(offset+1,testkeyvalue);
+    }
+
+    //allocate right node
+    // Allocate a new node. (The left node)
+    rc=AllocateNode(&xr_ptr);
+    if (rc) { return rc; }
+    rc=xl_node.Unserialize(buffercache,xr_ptr);
+    if (rc) { return rc; }
+    for (offset=b.info.numkeys/2+1;offset<b.info.numkeys;offset++){
+      rc=b.GetKeyVal(offset,testkeyvalue);
+      if (rc) {  return rc; }
+      xr_node.SetKeyVal(offset-(b.info.numkeys/2+1),testkeyvalue);
+    }
+    //deallocate original node
+    rc=DeallocateNode(node);
+    if (rc) { return rc; }
+
+  }else if (offset_keyvalue==b.info.numkeys/2-1){
+    // If the key to be inserted is the [m/2]th key, which is in neither xl or xr (It's stored in leaf node but is not a key in the current internal node)
+  }else{
+    // If the key to be inserted is in xr
+  }
+  return ERROR_UNIMPL;
+}
+
+// The function to split a leaf node
+ERROR_T BTreeIndex::SplitLeaf(const SIZE_T &node, const KEY_T &key, const VALUE_T &value){
+
+  return ERROR_UNIMPL;
+}
+
+
+// The helper function for insert. Can be called recursively.
+ERROR_T BTreeIndex::InsertHelper(const SIZE_T &node, const KEY_T &key, const VALUE_T &value)
+{
+  BTreeNode b;
+  ERROR_T rc;
+  SIZE_T offset;
+  KEY_T testkey;
+  SIZE_T ptr;
+
+  rc= b.Unserialize(buffercache,node);
+
+  if (rc!=ERROR_NOERROR) { 
+    return rc;
+  }
+
+  switch (b.info.nodetype) { 
+    case BTREE_ROOT_NODE:
+    case BTREE_INTERIOR_NODE:
+      // Scan through key/ptr pairs
+      //and recurse if possible
+      for (offset=0;offset<b.info.numkeys;offset++) { 
+        rc=b.GetKey(offset,testkey);
+        if (rc) {  return rc; }
+        if (key<testkey || key==testkey) {
+          // OK, so we now have the first key that's larger
+          // so we ned to recurse on the ptr immediately previous to 
+          // this one, if it exists
+          rc=b.GetPtr(offset,ptr);
+          if (rc) { return rc; }
+          return InsertHelper(ptr, key,value);
+        }
+      }
+      // if we got here, we need to go to the next pointer, if it exists
+      if (b.info.numkeys>0) { 
+        rc=b.GetPtr(b.info.numkeys,ptr);
+        if (rc) { return rc; }
+        return InsertHelper(ptr, key,value);
+      } else {
+        // There are no keys at all on this node, so nowhere to go
+        return ERROR_NONEXISTENT;
+      }
+      break;
+    case BTREE_LEAF_NODE:
+      // Scan through keys looking for matching value
+      for (offset=0;offset<b.info.numkeys;offset++) { 
+        rc=b.GetKey(offset,testkey);
+        if (rc) {  return rc; }
+        // we've found the place to insert the key and value
+        if (key<testkey || testkey==key) { 
+          //WRITE ME
+          if (b.GetNumSlotsAsLeaf()==0){
+            SplitLeaf(node);
+          }else{
+            // Move the memory after the insertion place one forward.
+            memmove(b.data+(keysize+valuesize)*offset+sizeof(SIZE_T),b.data+(keysize+valuesize)*(offset+1)+sizeof(SIZE_T),b.GetNumDataBytes()-sizeof(SIZE_T)-(keysize+valuesize)*offset);
+            b.SetKey(offset,key);
+            b.SetVal(offset,value);
+          }
+          return ERROR_UNIMPL;
+        }
+      }
+      return ERROR_NONEXISTENT;
+      break;
+    default:
+      // We can't be looking at anything other than a root, internal, or leaf
+      return ERROR_INSANE;
+      break;
+  }  
+
+  return ERROR_INSANE;
+}
+
 ERROR_T BTreeIndex::Insert(const KEY_T &key, const VALUE_T &value)
 {
   // WRITE ME
-  return ERROR_UNIMPL;
+  // We start at the superblock. 
+  // 1. Check if the current block is a leaf node. 
+  //     If not a leaf node.
+  //        do search (may use linear search for now, can write a separate function)
+  //            If currKey<key, then move to the right. Otherwise return the pointer of currKey. (next pointer to the right)
+  //            If we reached the end, then the key is larger than any other key in the current node. Insert into the pointer Which leads us to the next block in the linked list
+  //     If a leaf node,
+  //        If leaf contains <L keys, insert into the leaf at the correct position (using search)
+  //        If leaf is already full, then split the leaf.
+  //            Insert into leaf, pretending that the leaf has space for the new key/value.
+  //            Split x into 2 new leaves xl and xr. xl contains the l/2 smallest keys and xr contains the remaining l/2 keys. Let J be the minimum key in xr
+  //            Make a copy of J to be the parent of xl and xr. insert the copy into the parent node. (key=J, value=pointer to xl)
+  //        Splitting an internal node:
+  //            The difference is xl contains [m-2]-1 smallest keys and xr contains [m/2] largest keys. 
+  //            Note that the [m/2]th key J is not placed in xl or xr, but is used to be a key in parent node
+  //            Make J the parent of xl and xr, and push j together with its child pointers(to xl) into the parent of x. 
+  return InsertHelper(superblock_index,key,value);
 }
+
   
 ERROR_T BTreeIndex::Update(const KEY_T &key, const VALUE_T &value)
 {
